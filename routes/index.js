17799cd15cc712caa1c4958201721665ba824fda
William <williambecker321@gmail.com>
2021-11-12 15:49:38 +0100

    More of the previus

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,16 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
-router.get('/loggIn', function(req, res, next) {
+router.post('/', function(req, res, next) {
+    res.render('index');
+});
+
+router.get('/LoggedIn', function(req, res, next) {
+    res.render('Login');
+});
+
+router.post('/LoggedIn', function(req, res, next) {
+    console.log(res);
     res.render('Login');
 });
 
