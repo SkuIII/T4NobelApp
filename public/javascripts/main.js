abcc0686cf826797a8011c33784fdedb7b209444
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-11-12 13:21:29 +0100

    added 2 fetch for more data

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,16 +10,34 @@ fetch('/data/NominatedInfo')
         const NominatedInfo = resNominatedInfo;
         Load(NominatedInfo);
     });
+fetch('/data/Categories')
+    .then(response => {
+        return response.json();
+    })
+    .then(resCategoriesInfo => {
+        const CategoriesInfo = resCategoriesInfo;
+        Load(CategoriesInfo);
+    });
+fetch('/data/Students')
+    .then(response => {
+        return response.json();
+    })
+    .then(resStudentsInfo => {
+        const StudentsInfo = resStudentsInfo;
+        Load(StudentsInfo);
+    });
 
-const Load = (NominatedInfo) => {
+const Load = (NominatedInfo, CategoriesInfo, StudentsInfo) => {
     console.log(NominatedInfo);
+    console.log(CategoriesInfo);
+    console.log(StudentsInfo);
 
     const img = document.createElement('img');
     const h1 = document.createElement('h1');
 
-    img.src = NominatedInfo[0].record.fields.Nominerad1Bild[0].url;
+    img.src = NominatedInfo[0].record.fields.Picture[0].url;
 
-    h1.textContent = NominatedInfo[0].record.fields.Nominerad1;
+    h1.textContent = NominatedInfo[0].record.fields.Nominated;
 
     document.getElementById('main').appendChild(h1);
     document.getElementById('main').appendChild(img);
