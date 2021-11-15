712e445144befdb5b3396766a8e0e9f42f95d816
William <williambecker321@gmail.com>
2021-11-15 09:02:50 +0100

    Fixat merge

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,10 @@ router.post('/', function(req, res, next) {
     res.render('index');
 });
 
-<<<<<<< HEAD
 router.get('/anka', function(req, res, next) {
     res.render('login');
-=======
+});
+
 router.get('/LoggedIn', function(req, res, next) {
     res.render('Login');
 });
@@ -20,7 +20,6 @@ router.get('/LoggedIn', function(req, res, next) {
 router.post('/LoggedIn', function(req, res, next) {
     console.log(res);
     res.render('Login');
->>>>>>> 22597141545dd4dcbb5b6ad359ea9f9cd1ebcaa2
 });
 
 module.exports = router;
