09cd4ab0be9af8e8ec17f960b50610c8b17a36b0
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-11-16 09:54:50 +0100

    Impl. airtable data with js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -27,17 +27,33 @@ const Load = (res) => {
     const CategoryInfo = res[1];
     const StudentInfo = res[2];
 
-    console.log(NominatedInfo);
-    console.log(CategoryInfo);
-    console.log(StudentInfo);
+    // console.log(NominatedInfo);
+    // console.log(CategoryInfo);
+    // console.log(StudentInfo);
+
+    //JavaScript code to generate HTML for loginVote
+    const Rubrik = document.createElement('h1');
+    const längk = document.createElement('p');
+    Rubrik.textContent = 'hejsan';
+    längk.textContent = 'he';
+    Rubrik.appendChild(längk);
+    document.getElementById('anka').appendChild(Rubrik);
+    const pic1 = document.getElementById('picture1');
+    pic1.src = 'https://dl.airtable.com/.attachments/36014f6e2f08fa506ead8fa7b55c02b5/05d16d37/bild_2021-11-12_100959.png';
+    
 
     // Looping through nominated people by category
-    CategoryInfo.forEach(Category => {
-        console.log(Category.record.fields.Category);
+    CategoryInfo.forEach((Category, counterCategory) => {
+        
+        // console.log(Category.record.fields.Category);
 
         NominatedInfo.forEach(Nominated => {
             if (Nominated.record.fields.Category == Category.record.fields.Category){
-                console.log(Nominated.record.fields.Picture[0].url);
+                // console.log(Nominated.record.fields.Picture[0].url);
+
+                console.log('Category' + (counterCategory + 1) + "," + Nominated.record.fields.Nominated);
+                //Nominated, bio, name and Url
+
 
                 // const b = document.createElement('h1');
                 // b.textContent = 'HEJ JAG ÄR FETT SEG';
@@ -45,6 +61,8 @@ const Load = (res) => {
             
                 // console.log("Hej---------------" + NominatedInfo[0].record.fields.Picture[0].url);
                 // console.log("Hej---------------" + NominatedInfo[3].record.fields.Picture[0].url);
+
+                // Image url NominatedInfo[0].record.fields.Picture[0].url
 
             
             
