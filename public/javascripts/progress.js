20152ed377eb58f9f03b02d991a58fe464b5c34f
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-11-17 15:37:29 +0100

    added some more design to the ux

--- public/javascripts/progress.js
+++ public/javascripts/progress.js
@@ -1,21 +1,27 @@
 'use strict';
 
-var i = 1;
+
 function progress() {
-  if (i == 1) {
-    i = 2;
+  if (true) {
+
     var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval( frame, 100);
+    var width = 0;
+
+    var id = setInterval( frame, 10);
+
+    elem.style.width = width + "%";
+    elem.innerHTML = width  + "%";
+
     function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-        elem.innerHTML = width  + "%";
-      }
+     if (width >= 50) {
+    clearInterval(id);
+    // i = 0;
+    } else {
+    width++;
+    elem.style.width = width + "%";
+    elem.innerHTML = width  + "%";
+    }
+    console.log('All good with the progress')
     }
   }
 };
