7c118a5b06824f9e15fa38c2b91007050786c126
JohanK <klausenjohan23@gmail.com>
2021-11-22 13:57:31 +0100

    Combined voting system

--- public/javascripts/Vote.js
+++ public/javascripts/Vote.js
@@ -6,27 +6,41 @@ console.log('Vote.js is alive!');
 
 const divVoteClick = (event) => {
     const id = event.target.id;
-    
+
     let temp = id.split(',');
 
     const Category = temp[0];
     const Nominated = temp[1];
 
+    console.log(vote);
+
     vote.forEach((element, elementCounter) => {
         console.log(elementCounter + '--------------------------------------------------')
-        if (element.CategoryVoted == Category && UserVoteData[elementCounter] == 'Empty') {
+        if (VoteStatus == 'ToVote' && Category == element.CategoryVoted) {
             element.NominatedVoted = Nominated;
+            element.testing = 1;
             console.log('Du har nu röstat på ' + element.NominatedVoted + ' i ' + element.CategoryVoted)
-        } else
-        if (element.CategoryVoted == Category && UserVoteData[elementCounter] != 'Empty') {
+        }
+        if (VoteStatus == 'Voted') {
             console.log('Du har redan röstat i ' + element.CategoryVoted)
-            console.log(UserVoteData[elementCounter])
+        }
+    });
+
+    let counter = 0;
+
+    vote.forEach(Vote => {
+        if (Vote.testing == 1) {
+            counter++
+            if (counter == 3) {
+                document.getElementById('confirm-btn').disabled = false;
+            }
         }
     })
 }
 
 const btnConfirmClick = () => {
-    const url = 'https://shrouded-wave-16183.herokuapp.com/Vote';
+    document.getElementById('confirm-btn').disabled = true;
+    const url = document.URL;
 
     console.log('{"email":"' + email + '", "vote":' + JSON.stringify(vote) + '}');
 
