9a01fd23440afbd3a3107f4305beace238bada58
kevin wilusz <kwilusz10@gmail.com>
2021-11-23 09:41:42 +0100

    added comments

--- public/javascripts/Slideshow.js
+++ public/javascripts/Slideshow.js
@@ -19,16 +19,16 @@ const LoadNominatedInfo = (res) => {
     // console.log(NominatedInfo[0].record.fields.Nominated);
     // console.log(NominatedInfo[0].record.fields.Picture[0].url);
 
-    NominatedInfo.forEach(Nominee => {
-        console.log(Nominee.record.fields.Nominated);
-        console.log(Nominee.record.fields.Picture[0].url);
+    NominatedInfo.forEach(Nominee => {                  //Makes a new slide for every nominee in the airtable
+        console.log(Nominee.record.fields.Nominated);       //fetch nominees name from airtable
+        console.log(Nominee.record.fields.Picture[0].url);  //fetch nominees picture from airtable
 
-        let li = document.createElement('li');
-        li.id = Nominee.record.fields.Nominated;
+        let li = document.createElement('li');           
+        li.id = Nominee.record.fields.Nominated;        //makes every slide with different id. 
         document.getElementById('ol').appendChild(li);
 
         let img = document.createElement('img');
-        img.src = Nominee.record.fields.Picture[0].url;
+        img.src = Nominee.record.fields.Picture[0].url;  //makes every slide with different picture
         document.getElementById(Nominee.record.fields.Nominated).appendChild(img);
 
         let h1 = document.createElement('h1');
@@ -37,8 +37,8 @@ const LoadNominatedInfo = (res) => {
         
     });
 
-    const slide = document.createElement('script');
-    slide.src = 'javascripts/ism-2.2.min.js';
-    document.getElementById('content').appendChild(slide);
+    const slide = document.createElement('script');         //Makes the javascript to read in after
+    slide.src = 'javascripts/ism-2.2.min.js';                //the javascript for html and airtable info is collected       
+    document.getElementById('content').appendChild(slide);          
     
 }
