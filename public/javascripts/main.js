688d0ab94d848e2baa79780e46be2778db7d908f
JohanK <klausenjohan23@gmail.com>
2021-11-26 12:31:34 +0100

    fixed a bug with cols wont open

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -132,11 +132,11 @@ acceptingText.appendChild(acceptChoice);
 const showBio = (sender) => {
     const ID = sender.target.name;
     const anka = document.getElementById(ID);
-    if (anka.className == 'col-sm-3 border-phatHome mx-3 p-0 text-center') {
-        anka.className = 'col-sm-3 border-phatHome-hover mx-3 p-0 text-center';
+    if (anka.className == 'col-sm-3 border-phatHome mx-3 my-2 p-0 text-center') {
+        anka.className = 'col-sm-3 border-phatHome-hover mx-3 my-2 p-0 text-center';
         sender.target.textContent = 'VISA MINDRE';
-    } else if (anka.className == 'col-sm-3 border-phatHome-hover mx-3 p-0 text-center') {
-        anka.className = 'col-sm-3 border-phatHome mx-3 p-0 text-center';
+    } else if (anka.className == 'col-sm-3 border-phatHome-hover mx-3 my-2 p-0 text-center') {
+        anka.className = 'col-sm-3 border-phatHome mx-3 my-2 p-0 text-center';
         sender.target.textContent = 'VISA MERA / RÖSTA';
     }
 }
